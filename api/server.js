ef86063839bd609d86b2684c3e670462825a387b
James Karpinski <jameskarpinski@FM180.wcs.wells-cathedral-school.com>
2016-12-05 12:22:45 +0000

    example api Server code written

--- api/server.js
+++ api/server.js
@@ -1,1 +1,26 @@
-"use strict"
+"use strict";
+var express    = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.user(bodyParser.json({limit: "50mb", extended: true}))
+
+//allow requests from different domains
+app.use(function(req, res, next){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
+  next();
+});
+
+port = process.env.port || 8081;
+router = express.Router();
+
+//test route to test that everything's working ok
+router.get("/", function(req, res){
+  res.json({ message: "API incoming!"});
+});
+
+app.listen(port);
+console.log("example API is running on port 8081")
