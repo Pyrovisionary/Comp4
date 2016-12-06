8959470097fe35d2e48ef697655f28848b5091e1
James Karpinski <jameskarpinski@FM180.wcs.wells-cathedral-school.com>
2016-12-06 11:38:31 +0000

    Started database, got server working

--- api/server.js
+++ api/server.js
@@ -1,21 +1,23 @@
 "use strict";
 var express    = require("express");
+var app        = express();
 var bodyParser = require("body-parser");
+var morgan     = require("morgan");
 
-var app = express();
+
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
+app.use(bodyParser.json({limit: "50mb"}));
 
 app.use(morgan("dev"));
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.json({limit: "50mb", extended: true}))
 
 //allow requests from different domains
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
   next();
 });
 
-var port = process.env.port || 8081;
+var port = process.env.port || 8080;
 var router = express.Router();
 
 //test route to test that everything's working ok
@@ -23,6 +25,6 @@ router.get("/", function(req, res){
   res.json({ message: "API incoming!"});
 });
 
-app.use("api", router);
+app.use("/api", router);
 app.listen(port);
-console.log("example API is running on port 8081")
+console.log("example API is running on port 8080");
