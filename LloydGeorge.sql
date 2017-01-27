606a430e7b07f015d8c179cdb0c4338d2ab859c0
James Karpinski <jameskarpinski@FM180.wcs.wells-cathedral-school.com>
2017-01-27 10:40:14 +0000

    Added CRUD methods

---
+++ LloydGeorge.sql
@@ -0,0 +1,59 @@
+DROP DATABASE LloydGeorge;
+CREATE DATABASE LloydGeorge;
+
+USE LloydGeorge;
+
+CREATE TABLE IF NOT EXISTS classes(
+	classid integer primary key auto_increment not null,
+    classname varchar(20) not null
+);
+
+CREATE TABLE IF NOT EXISTS users(
+	userid integer primary key auto_increment not null,
+    username varchar(20) not null,
+    forename varchar(20) not null,
+    surname varchar(20) not null,
+    email varchar(40) not null,
+    teacher boolean
+);
+
+CREATE TABLE IF NOT EXISTS classuserlink (
+	linkid integer primary key auto_increment not null,
+    classid integer not null,
+    userid integer not null,
+    FOREIGN KEY (classid) REFERENCES classes(classid),
+    FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE IF NOT EXISTS portfolios(
+	portfolioid integer primary key auto_increment not null,
+    userid integer not null,
+    portfolioname varchar(20),
+    FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE IF NOT EXISTS stocknames(
+	stockid integer primary key auto_increment not null ,
+    stockticker varchar(4) not null,
+    stockname varchar(20) not null
+);
+
+CREATE TABLE IF NOT EXISTS stockhistory(
+	stockhistoryid integer primary key auto_increment not null, 
+    stockid integer not null,
+    stockvalue integer not null,
+    sampletime TIMESTAMP not null,
+    volume integer not null,
+    FOREIGN KEY (stockid) REFERENCES stocknames(stockid)
+);
+
+CREATE TABLE IF NOT EXISTS portfoliostocklink(
+	portfoliostocklinkid integer primary key auto_increment not null,
+    portfolioid integer not null,
+    stockid integer not null,
+	buyprice integer not null,
+    volume integer not null,
+    purchasedatetime TIMESTAMP,
+    FOREIGN KEY (portfolioid) REFERENCES portfolios(portfolioid),
+    FOREIGN KEY (stockid) REFERENCES stocknames(stockid)
+);
--- api/server.js
+++ api/server.js
@@ -4,6 +4,21 @@ var app        = express();
 var bodyParser = require("body-parser");
 var morgan     = require("morgan");
 var mysql      = require('mysql');
+var config     = require('./config');
+
+var pool        = mysql.createPool({
+  connectionLimit : 30,
+  host            : config.mysql.host,
+  user            : config.mysql.user,
+  password        : config.mysql.password,
+  database        : config.mysql.db
+});
+
+
+// load routes
+var auth            = require('./routes/auth');
+var departments     = require('./routes/departments');
+var ensembles       = require('./routes/ensembles');
 
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 app.use(bodyParser.json({limit: "50mb"}));
@@ -25,56 +40,230 @@ router.get("/", function(req, res){
   res.json({ message: "API incoming!"});
 });
 
-//TODO: put mysql config details in a config.json file
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'Jamesk',
-  password : 'Jamesk1',
-  database : 'Database'
-});
-
 //CRUD routes for widgets
-router.route('/stocks')
-  //Should get all widgets
-  .get( function(req, res){
-      //Query the database for all stocks - returns all stocks in a 'rows' object
-      /*
-      connection.connect();
-      connection.query('Select * FROM Stocks ORDER BY Name', function(err, rows, fields){
-        if (err) throw err;
-
-        res.json(rows);
-      })
-      connection.end(); */
-      var testArray = [
-        {
-          'idStock': "1",
-          'StockName': 'Google'
-        },
-        {
-          'idStock': "2",
-          'StockName': 'Apple'
-        }
-      ]
-
-      res.json(testArray)
-  })
-  .post()
-  .put()
-  .delete();
-
-router.route('/stocks/:stockid')
-  .get(function(req,res){
-    connection.connect();
-
-    connection.query('SELECT * FROM stocks WHERE stockid = ' + req.params.stockid, function(err, rows, fields){
+router.route('/classes')
+  //Get all classes (Just Id's and names)
+  .get(function(req, res){
+    pool.query('SELECT * FROM classes', function(err, rows, fields){
       if (err) console.log(err);
       res.json(rows);
     });
-
-    connection.end();
+  })
+  //Create a class
+  .post(function(req, res){
+    pool.query('INSERT INTO classes (classname) VALUES(\''  + req.body.classname +'\')', function(err, rows, fields){
+      if(err) console.log(err);
+      res.json("Class " +req.body.classname+ " created")
+    });
   });
 
+
+router.route('/classes/:classid')
+  //Get a specific class
+  .get(function(req, res){
+    pool.query('SELECT * FROM classes INNER JOIN classuserlink ON classes.classid = classuserlink.classid WHERE classid = ' + req.params.classid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  })
+  //Update a specific class
+  .put(function(req,res){
+    pool.query('UPDATE classes SET classname="'+ req.body.classname +'" WHERE classid =' + req.params.classid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json("Classname updated");
+    });
+
+  })
+  //Add a pupil to a class
+  .post(function(req, res){
+    pool.query('INSERT INTO classuserlink (classid, userid) VALUES(\''  + req.body.classid +'\' \'' + req.body.userid +'\')', function(err, rows, fields){
+      if(err) console.log(err);
+      res.json("User " +req.body.userid+ " added to class " + req.body.classid )
+    });
+  })
+  //Delete a specific class
+  .delete(function(req,res){
+    console.log("Attempting to delete");
+    pool.query('DELETE FROM classes WHERE classid = ' + req.params.classid +"; DELETE FROM classuserlink WHERE classid = " +req.params.classid, function(err, rows,fileds){
+      if(err) console.log(err);
+      res.json({message: "Class " + req.params.classid + "deleted successfully!"});
+    });
+  });
+
+
+router.route('/stocknames')
+    .get(function(req, res){
+        pool.query('Select TOP 1 sampletime FROM stockhistory ORDER BY sampletime DESC', function(err, rows, fields){
+          if (err) console.log(err);
+          var newestsampletime = rows[0].sampletime;
+          pool.query('Select * FROM stocknames INNER JOIN stockhistory ON stocknames.stockid=stockhistory.stockid WHERE sampletime =' + newestsampletime + ' ORDER BY stockname', function(err, rows, fields){
+            if (err) console.log(err);
+            res.json(rows);
+          });
+        });
+    })
+    //Add stock(By name)
+    .post(function(req, res){
+      pool.query('INSERT INTO stockname (stockticker, stockname) VALUES(\''  + req.body.stockticker + '\', \'' + req.body.stockname + '\')', function(err, rows, fields){
+        if(err) console.log(err);
+        res.json("Stock " +req.body.stockname+ " created")
+      });
+    });
+
+
+router.route('/stocknames/:stockid')
+  //get one stock
+  .get(function(req,res){
+    pool.query('SELECT * FROM stocknames WHERE stockid=' + req.params.stockid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows[0]);
+    });
+  })
+  //Update a stock
+  .put()
+  //Delete a stock
+  .delete(function(req,res){
+    console.log("Attempting to delete");
+    pool.query('DELETE FROM stocknames WHERE stockid = ' + req.params.stockid, function(err, rows,fileds){
+      if(err) console.log(err);
+      res.json({message: "Stock" + req.params.stockid + "deleted successfully!"});
+    });
+  });
+
+
+router.route('/stockhistory')
+.post(function(req, res){
+  pool.query('INSERT INTO stockhistory (stockid, stockvalue, volume) VALUES(\''  + req.body.stockid + '\', \'' + req.body.stockvalue + '\', \''+req.body.volume + '\')', function(err, rows, fields){
+    if(err) console.log(err);
+    res.json("Stock " +req.body.stockid+ " updated")
+  });
+});
+
+
+router.route('/users')
+  //Get all users
+  .get(function(req, res){
+    pool.query('Select * FROM users', function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  })
+  //Add user
+  .post(function(req, res){
+    pool.query('INSERT INTO users (username, forename, surname, email, teacher) VALUES(\''  + req.body.username + '\', \'' + req.body.forename + '\', \'' + req.body.surname + '\', \'' + req.body.email + '\', \'' + req.body.teacher +'\')', function(err, rows, fields){
+      if(err) console.log(err);
+      res.json("User " +req.body.username+ " created")
+    });
+  });
+
+router.route('/users/:userid')
+  //Get a user
+  .get(function(req,res){
+    pool.query('SELECT * FROM users WHERE userid=' + req.params.userid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows[0]);
+    });
+  })
+  //Update a user
+  .put(function(req, res){
+    pool.query('UPDATE users SET'+req.body.changeddatatype+'=' +req.body.changeddata+ 'WHERE userid = ' + req.params.userid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json("User " + req.params.userid + " updated");
+    });
+  })
+  //Delete a user
+  .delete(function(req,res){
+    console.log("Attempting to delete");
+    pool.query('DELETE FROM users WHERE userid = ' + req.params.userid, function(err, rows,fileds){
+      if(err) console.log(err);
+      res.json({message: "User deleted successfully!"});
+    });
+  });
+
+
+router.route('users/:userid/:classes')
+//Get a user's classes
+  .get(function(req, res){
+    pool.query('Select * FROM classes WHERE userid = ' + req.params.userid , function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  });
+
+router.route('/portfolios')
+//Get all portfolios
+  .get(function(req, res){
+    pool.query('Select * FROM portfolios', function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  })
+  //Create a new portfolio
+  .post(function(req, res){
+    pool.query('INSERT INTO portfolios (userid, portfolioname) VALUES(\''  + req.body.userid + '\', \'' + req.body.portfolioname +'\')', function(err, rows, fields){
+      if(err) console.log(err);
+      res.json("Portfolio " +req.body.portfolioname+ " created")
+    });
+  });
+
+
+router.route('/portfolios/:portfolioid')
+  //Get a specific portfolio
+  .get(function(req, res){
+    pool.query('SELECT * FROM portfolios WHERE portfolioid=' + req.params.portfolioid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows[0]);
+    });
+  })
+  //Update a specific portfolio
+  .put(function(req, res){
+  })
+  //Delete a specific portfolio
+  .delete(function(req,res){
+    console.log("Attempting to delete");
+    pool.query('DELETE FROM portfolios WHERE portfolioid = ' + req.params.portfolioid, function(err, rows,fileds){
+      if(err) console.log(err);
+      res.json({message: "Portfolio deleted successfully!"});
+    });
+  });
+
+router.route('/portfolios/:portfolioid/:stockid')
+  //Add a specific stock to a specific portfolio
+  .post(function(req, res){
+    pool.query('SELECT TOP 1 FROM stockhistory WHERE stockid = ' + req.params.stockid +' ORDER BY sampletime DESC', function(err, rows, fields){
+      if(err) console.log(err);
+      var buyprice = rows[0].stockvalue
+      pool.query(' INSERT INTO portfoliostocklink (portfolioid, stockid, buyprice, volume) VALUES(\''  + req.body.portfolioid + '\', \'' + req.body.stockid + '\', \'' + buyprice + '\', \''+ req.body.volume+'\')', function(err, rows, fields){
+        if(err) console.log(err);
+        res.json("Stock " +req.body.stockid+ " added to portfolio " + req.body.portfolioid)
+      });
+    });
+  });
+
+
+router.route('/portfolios/:userid')
+  //Get a user's portfolio
+  .get(function(req, res){
+    pool.query('SELECT * FROM portfolios WHERE userid=' + req.params.userid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  });
+
+
+router.route('/portfolios/:userid/:portfolioid')
+  //Get a specific portfolio + all the portfoliostocklink data
+  .get(function(req, res){
+    pool.query('SELECT * FROM portfolios INNER JOIN portfoliostocklink ON portfolios.'+req.params.portfolioid+' = portfoliostocklink.'+req.params.portfolioid+'  AND WHERE userid = ' + req.params.userid, function(err, rows, fields){
+      if (err) console.log(err);
+      res.json(rows);
+    });
+  })
+
+
 app.use("/api", router);
+app.use('/api/', auth);
+app.use('/api/', departments);
+app.use('/api/', ensembles);
 app.listen(port);
 console.log("example API is running on port 8080");
